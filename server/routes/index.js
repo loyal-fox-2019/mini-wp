b9c958a1526e223b78bd189e9086be0631d4eb37
Trayand <patradyn@gmail.com>
2020-01-14 15:57:54 +0700

    delete commitan zoey :)

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const routes = require('express').Router()
-const ArticleRoutes = require('./articleRoutes')
-const AuthorRoutes = require('./authorRoutes')
-
-routes.use('/author', AuthorRoutes)
-routes.use('/article', ArticleRoutes)
-
-module.exports = routes
