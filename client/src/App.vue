2651b7beb0b05551a96a9ac7c0ee0722483eb87b
deprak <dedprakasa@gmail.com>
2020-01-14 15:11:38 +0700

    add responsive navbar

---
+++ client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <TheNavbar/>
+
+</template>
+
+<script>
+import TheNavbar from './components/TheNavbar'
+export default {
+  name: 'app',
+  components: {
+    TheNavbar
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  created () {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
