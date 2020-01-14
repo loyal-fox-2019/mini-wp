686b139d37532bd88c045c15c4dd2b5356259b5f
fragilities <55373574+fragilities@users.noreply.github.com>
2020-01-14 17:19:03 +0700

    Delete authorRoutes.js

--- server/routes/authorRoutes.js
+++
@@ -1,8 +0,0 @@
-const routes = require('express').Router()
-const AuthorController = require('../controllers/AuthorController')
-
-routes.post('/register', AuthorController.register)
-routes.post('/login', AuthorController.login)
-routes.get('/nopenopenope', AuthorController.read)
-
-module.exports = routes
