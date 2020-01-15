6b312510b2d462df24e023ad031aa6524ec68357
sukmabrahmantya <brahmantyasukma@gmail.com>
2020-01-15 15:39:55 +0700

    adding article model schema in server side

--- server/models/article.js
+++ server/models/article.js
@@ -1,1 +1,29 @@
 'use strict'
+
+const { Schema, model } = require('mongoose')
+const ObjectId = Schema.Types.ObjectId
+
+const articleSchema = new Schema ({
+  title: {
+    type: String,
+    required: [true, 'Title cannot be empty']
+  },
+  content: {
+    type: String,
+    required: [true, 'Content cannot be empty']
+  },
+  featured_image: {
+    type: String
+  },
+  tags: {
+    type: [String]
+  },
+  userId : {
+    type: ObjectId,
+    ref: 'User'
+  }
+}, { timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } })
+
+const Article = model('Article', articleSchema)
+
+module.exports = Article
