727b27c045a46514d4db144b3a00c4e595ff4d88
kirwanm <kirwanm@yahoo.com>
2020-01-15 19:29:01 +0700

    Modified mongodb connection

--- server/mongoose/index.js
+++ server/mongoose/index.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 // const uri = `mongodb://localhost:27017/mini-wp`;
-const uri = `mongodb+srv://kirwanm:${process.env.MONGODB_PASS}@projects-og8m8.gcp.mongodb.net/test?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://kirwanm:1v0TY56$@projects-og8m8.gcp.mongodb.net/test?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
