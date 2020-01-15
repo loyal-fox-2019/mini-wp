14cb2eb0e486c93dfb5e90dfbf1175eb1409c85a
sukmabrahmantya <brahmantyasukma@gmail.com>
2020-01-15 15:46:19 +0700

    adding authentication in midlewares server side

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -1,1 +1,14 @@
 'use strict'
+
+const { verifyToken } = require('../helpers/verifyToken')
+
+module.exports = (req, res, next) => {
+  try {
+    let token = req.headers.token
+    const payload = verifyToken(token)
+    req.decoded = payload
+    next ()
+  } catch (err) {
+    next (err)
+  }
+}
