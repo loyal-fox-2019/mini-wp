b93383a770ca962200c0c934f0c647cbf23370f4
sukmabrahmantya <brahmantyasukma@gmail.com>
2020-01-15 16:16:25 +0700

    adding app js to setting config in server side

--- server/app.js
+++ server/app.js
@@ -1,2 +1,29 @@
 'use strict'
 
+if(process.env.NODE_ENV == 'development') require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+// const router = require('./routes')
+// const errorHandler = require('./middlewares/errorHandler')
+const databaseAccess = process.env.MONGO_URI
+
+mongoose.connect(databaseAccess, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true, 
+  useFindAndModify: false
+}, function(err) {
+  if(err) console.log('database is an Error')
+  else console.log(`database in an active ${databaseAccess}`)
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// app.use('/', router)
+// app.use(errorHandler)
+
+module.exports = app
