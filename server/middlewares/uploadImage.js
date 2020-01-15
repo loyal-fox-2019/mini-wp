bfc861ffa2c5169353a26e87ea53dbea6b824383
ilhamabdulm <a.marzmellow21@gmail.com>
2020-01-15 18:56:20 +0700

    edit upload image middleware

--- server/middlewares/uploadImage.js
+++ server/middlewares/uploadImage.js
@@ -5,7 +5,7 @@ module.exports = gcsUpload({
       fileSize: 1e6 // in bytes
     },
     gcsConfig: {
-      keyFilename: '/home/ilhamabdulm/Documents/Hacktiv8 Batch 38/phase_2/p2_w2/mini-wp/server/middlewares/mini-wp-743d656d0977.json',
+      keyFilename: 'mini-wp-743d656d0977.json',
       bucketName: 'storage-miniwp-mashumuro'
     }
 })
