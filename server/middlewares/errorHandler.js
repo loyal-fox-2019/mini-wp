89a1eea82f607c1d900b5644045f6dc86dc9f9a3
sukmabrahmantya <brahmantyasukma@gmail.com>
2020-01-15 16:04:39 +0700

    adding error handler in middlewares server side

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,1 +1,29 @@
 'use strict'
+
+module.exports = (err, req, res, next) => {
+  console.log(err)
+
+  let status, message
+  switch (err.name) {
+    case "ValidatorError":
+      let mongooseError = err.errors
+      status = 400
+      message = []
+      for(let key in mongooseError) {
+        message.push(mongooseError[key].message)
+      }
+      break;
+
+    case "JsonWebTokenError":
+      status = 401
+      message = "Invalid Access Token"
+      break;
+
+    default:
+      status = err.status || 500
+      message = err.message || 'Internal Server Error'
+      break;
+  }
+
+  res.status(status).json({ message })
+}
