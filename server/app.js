aa4c5e81f360b66a51b0d8746011ffba85928767
alvihrhp <rafael.hrhp@yahoo.com>
2020-01-15 16:54:27 +0700

    Move dotenv to production

--- server/app.js
+++ server/app.js
@@ -1,8 +1,7 @@
 'use strict';
 
-if(process.env.NODE_ENV === 'development') {
-    require('dotenv').config();
-}
+
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
