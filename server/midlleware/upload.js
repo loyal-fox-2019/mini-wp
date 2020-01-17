d2c5e58c60c0ff7f039af1cdec63b6e67f533ef7
Riko Orlando <rikoorlando76@gmail.com>
2020-01-16 20:22:16 -0500

    solve upload

--- server/midlleware/upload.js
+++ server/midlleware/upload.js
@@ -6,7 +6,7 @@ const upload = gcsUpload({
         fileSize: 1e6
     },
     gcsConfig:{
-        keyFilename: 'token.json',
+        keyFilename: 'hack-tiv8-project-4bdb4ee78a06.json',
         bucketName: 'article-image-miniwp'
     }
 })
