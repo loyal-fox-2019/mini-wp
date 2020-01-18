5a946ff15c7394476512c904074cbee9d2bfdb96
sukmabrahmantya <brahmantyasukma@gmail.com>
2020-01-18 21:43:29 +0700

    adding login in user controller server side

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,1 +1,40 @@
 'use strict'
+
+const { User } = require('../models')
+const bcrypt = require('bcryptjs')
+const { generateToken, verifyToken } = require('../helpers/verifyToken')
+
+class userController {
+  static login(req, res, next) {
+    const { email, password } = req.body
+
+    User
+      .findOne({ email })
+      .then(user => {
+        if(user) {
+          const valid = bcrypt.compareSync(password, user.password)
+          if(valid) {
+            const token = generateToken({ id: user._id })
+            const data = {
+              username: user.username,
+              token
+            }
+            res.status(200).json(data)
+          } else {
+            throw ({
+              status: 400,
+              message: 'Your email/password is wrong'
+            })
+          }
+        } else {
+          throw ({
+            status: 400,
+            message: 'Your email is not registered'
+          })
+        }
+      })
+      .catch(next)
+  }
+}
+
+module.exports = userController
