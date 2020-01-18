fd7b8cebf04bbeccbb8629629041e805d559ff3a
michaelwp <michael.wenceslaus@gmail.com>
2020-01-18 09:37:00 +0700

    update client configuration

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,10 +1,7 @@
-// import dotenv from 'dotenv';
 import axios from 'axios';
 
 const baseURLProd = 'http://35.184.99.60:3000';
 const baseURLDev = 'http://localhost:3000';
-
-// dotenv.config();
 
 const instance = axios.create({
     baseURL: baseURLDev
--- client/src/main.js
+++ client/src/main.js
@@ -5,11 +5,8 @@ import GSignInButton from 'vue-google-signin-button';
 import VueQuill from 'vue-quill';
 import GAuth from 'vue-google-oauth2';
 
-const googleClientIdProd = '1077663190419-heca6gdre9f5031fdtd57m2uts9u64jj.apps.googleusercontent.com';
+const googleClientIdProd = '1077663190419-3fl32g86v8evqq5ud7ivlusngc81k965.apps.googleusercontent.com';
 const googleClientIdDev = '1077663190419-fkbutpat7sloejjac79gs8tnfa1bmi3k.apps.googleusercontent.com';
-// import dotenv from 'dotenv';
-//
-// dotenv.config();
 
 const gauthOption = {
     clientId: googleClientIdDev
