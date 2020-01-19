5a61a7e467b8efebe20d20f0f6debb962662681c
Immanuel Pandiangan <nuelpan@gmail.com>
2020-01-20 02:23:28 +0700

    add api documentation

--- client/helpers/axios.js
+++ client/helpers/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const server = axios.create({
   baseURL: `http://localhost:3000`
-  // baseURL: `http://ec2-18-136-102-8.ap-southeast-1.compute.amazonaws.com`  
+  // baseURL: `http://server-miniwp.nuelpan.com/`  
 })
 
 export default server
