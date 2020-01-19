328e132b0514e3aa9e18a0a31e67f4a2ee720720
EddyPermana22 <eddy.permana.sari@gmail.com>
2020-01-19 14:19:17 +0700

    create user model & setup password encryption using mongoose pre save hooks

--- server/app.js
+++ server/app.js
@@ -20,7 +20,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch((err) => console.log('failed to connect Database', err))
 
 app.get('/', (req, res, next) => {
-    res.send('server ready')
+    res.status(200).json({
+        message: `Server Ready`
+    })
 })
 
 app.use(router)
---
+++ server/model/user.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const bcyript = require('bcryptjs')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        validate: {
+            validator: function (value) {
+                return Users.findOne({
+                    email: value
+                })
+                    .then((user) => !!user)
+            },
+            message: props => `${props.value} already registred, please try another email address!`
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    }
+})
+
+userSchema.pre('save', function (next) {
+    this.password = bcyript.hashSync(this.password)
+    next()
+})
+
+const Users = mongoose.model(`Users`, userSchema)
+
+module.exports = Users
