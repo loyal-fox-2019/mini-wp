16accfd016c71c30249619aac9b20735088d9599
dzakki <dzakkiaz7@gmail.com>
2020-01-19 23:51:51 +0700

    reset repo

--- server/middlewares/ErrorHander.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (err, req, res, next) {  
-    console.log(err)
-    res.status(500).json({
-        message: 'internal server error'
-    })
-}
--- server/routes/index.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router()
-const PostRoutes = require('./PostRoutes')
-router.use('/posts', PostRoutes)
-
-module.exports = router
