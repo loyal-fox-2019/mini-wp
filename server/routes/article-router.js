2dce31a42915f3fd0aeb9ab9572d5d5334e0797d
didadadida93 <didadadida93@gmail.com>
2020-01-19 16:04:46 +0700

    update server

--- server/routes/article-router.js
+++ server/routes/article-router.js
@@ -2,10 +2,21 @@ const router = require('express').Router()
 const ArticleController = require('../controllers/article-controller')
 const { authenticate, authorize } = require('../middlewares/auth')
 
+const gcsUpload = require('gcs-upload')
+const uploadImage = gcsUpload({
+  limits: {
+    fileSize: 0.5e7, // in bytes => 5MB
+  },
+  gcsConfig: {
+    keyFilename: process.env.KEY_FILENAME,
+    bucketName: process.env.BUCKET_NAME,
+  },
+})
+
 router.get('/', ArticleController.getAllArticles)
 router.get('/by-tag', ArticleController.getAllArticlesByTags)
 router.use(authenticate)
-router.post('/', ArticleController.createArticle)
+router.post('/', uploadImage.single('featuredImage') ArticleController.createArticle)
 router.use('/:articleId', authorize)
 router.patch('/:articleId', ArticleController.editArticle)
 router.delete('/:articleId', ArticleController.deleteArticle)
